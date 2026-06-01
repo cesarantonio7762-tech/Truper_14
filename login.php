303cbc7499e8be7ff065c15de96a4cf1839071f7
cesarantonio7762-tech <cesarantonio7762@gmail.com>
2026-06-01 09:35:40 -0600

    mensaje del cambio

--- login.php
+++ login.php
@@ -27,7 +27,7 @@ if ($_POST) {
 <html lang="es">
 <head>
 <meta charset="UTF-8">
-<title>inicio de sesion</title>
+<title>LOGIN</title>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
